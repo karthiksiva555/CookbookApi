0fc1fb691c5fc258cfe556785423b46688582e5a
Siva Narisetty <sivanarisetty@geotab.com>
2023-11-11 20:45:35 -0500

    Add swagger to help testing endpoints

--- CookbookApi/Program.cs
+++ CookbookApi/Program.cs
@@ -1,10 +1,19 @@
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 app.UseRouting();
 app.UseEndpoints(endpoints => {
     endpoints.MapControllers();
 });
-
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.RoutePrefix = string.Empty;
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "Version 1");
+    });
+}
 app.Run();
